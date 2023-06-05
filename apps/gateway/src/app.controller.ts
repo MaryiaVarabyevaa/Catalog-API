95f4c6a668ccb2d57aaa95786e0483a0ce06c560
Varabyova Maryia <varabyovamaryia@Varabyovas-MacBook-Air.local>
2023-06-05 10:29:07 +0300

    feat: add rmq settings

--- apps/gateway/src/app.controller.ts
+++ apps/gateway/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { GatewayService } from './gateway.service';
+import {Controller, Get, Inject, LoggerService} from '@nestjs/common';
+import {WINSTON_MODULE_NEST_PROVIDER} from "nest-winston";
+import {AmqpConnection} from "@golevelup/nestjs-rabbitmq";
 
 @Controller()
-export class GatewayController {
-  constructor(private readonly gatewayService: GatewayService) {}
-
+export class AppController {
+  constructor(
+      @Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService,
+      private readonly amqpConnection: AmqpConnection,
+      ) { }
   @Get()
-  getHello(): string {
-    return this.gatewayService.getHello();
+  async getHello(): Promise<string> {
+    this.logger.log('Calling getHello function')
+    await this.amqpConnection.publish('auth', 'auth-route', { data: { id: 1 } });
+    console.log('msg published', 'auth', 'auth-route', { data: { id: 1 } });
+    return 'Hello World!';
   }
 }
--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,10 +1,28 @@
-import { Module } from '@nestjs/common';
-import { GatewayController } from './gateway.controller';
-import { GatewayService } from './gateway.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {ConfigModule} from "@nestjs/config";
+import {WinstonModule} from "nest-winston";
+import {winstonConfig} from "@app/common";
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 
 @Module({
-  imports: [],
-  controllers: [GatewayController],
-  providers: [GatewayService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: './apps/gateway/.env',
+    }),
+    WinstonModule.forRoot(winstonConfig),
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      exchanges: [
+        {
+          name: 'auth',
+          type: 'topic',
+        },
+      ],
+      // uri: 'amqp://127.0.0.1',
+      uri: 'amqp://rmq',
+    }),
+  ],
+  controllers: [AppController],
 })
-export class GatewayModule {}
+export class AppModule {}
